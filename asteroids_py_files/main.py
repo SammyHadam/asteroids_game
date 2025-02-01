7fc5b10b168a936437a93abffb089210a01a6a70
SammyHadam <sammyhadams@gmail.com>
2025-02-01 12:47:26 -0600

    added the spawning of asteroids with no collision just yet

--- asteroids_py_files/main.py
+++ asteroids_py_files/main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -11,16 +13,21 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
-    updateble = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
-    Player.containers = (updateble, drawable)
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     
 
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
 
     player = Player(x, y)
+    asteroid_field = AsteroidField()
     
     while True:
         for event in pygame.event.get():
@@ -28,7 +35,7 @@ def main():
                 return
         
         #player.update(dt)
-        updateble.update(dt)
+        updatable.update(dt)
         
         screen.fill("black")
 
